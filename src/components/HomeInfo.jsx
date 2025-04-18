714e98ea1cbd4b11c426c8ffc75268471d012fcf
Maihar <maihar7051@gmail.com>
2025-04-18 11:02:30 +0530

    edited the name

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -7,7 +7,7 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
         Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Maihar</span>
+        <span className='font-semibold mx-2 text-white'>Maihar Shrivastava</span>
         👋
         <br />
         Driving Smart Solutions with Machine Learning and Full Stack Development
